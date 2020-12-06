444962f8cb10a5f01cb210b33a9f7b2486a001d9
Shrey <shrey.dabhi23@gmail.com>
2020-12-06 14:17:42 +0530

    update editor options

--- editor.js
+++ editor.js
@@ -1,11 +1,12 @@
 chrome.storage.local.get(["JSONExtens1ondata"], (res) => {
     require.config({ paths: { 'vs': 'monaco/min/vs' } });
     require(['vs/editor/editor.main'], function () {
-        monaco.editor.setTheme('vs-dark');
         var editor = monaco.editor.create(document.getElementById('container'), {
             value: JSON.stringify(res.JSONExtens1ondata, null, "\t"),
             language: 'json',
-            readOnly: true
+            readOnly: true,
+            theme: 'vs-dark',
+            scrollBeyondLastLine: false
         });
         window.addEventListener("resize", function () {
             editor.layout();
