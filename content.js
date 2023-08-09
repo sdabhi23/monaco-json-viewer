5ff3a535b5642e82ed86376062f27b25ccfd5134
Shrey <shrey.dabhi23@gmail.com>
2023-08-09 11:43:08 +0530

    code formatting

--- content.js
+++ content.js
@@ -1,49 +1,46 @@
 /*jshint eqeqeq:true, forin:true, strict:true */
 /*global chrome, console */
 (() => {
+  "use strict";
 
-    "use strict";
+  const loadJsonViewer = () => {
+    // First, check if it's a PRE and exit if not
+    var bodyChildren = document.body.childNodes;
+    var pre = bodyChildren[0];
+    console.log("ready");
+    if (bodyChildren.length === 1 || pre.tagName === "PRE") {
+      // This is a 'plain text' page (just a body with one PRE child).
+      // It might be JSON/JSONP, or just some other kind of plain text (eg CSS).
 
-    const loadJsonViewer = () => {
+      // Hide the PRE immediately (until we know what to do, to prevent FOUC)
+      pre.style.display = "none";
+      var isJSON = true;
 
-        // First, check if it's a PRE and exit if not
+      try {
+        const JSONdata = JSON.parse(pre.innerText);
+        chrome.storage.local.set({ JSONExtens1ondata: JSONdata });
+      } catch (error) {
+        isJSON = false;
+        pre.style.display = "initial";
+      }
 
-        var bodyChildren = document.body.childNodes;
-        var pre = bodyChildren[0];
-        console.log("ready")
-        if (bodyChildren.length === 1 || pre.tagName === 'PRE') {
-            // This is a 'plain text' page (just a body with one PRE child).
-            // It might be JSON/JSONP, or just some other kind of plain text (eg CSS).
+      if (isJSON) {
+        var editor_node = document.createElement("iframe");
+        editor_node.setAttribute("src", chrome.runtime.getURL("editor.html"));
+        editor_node.setAttribute(
+          "style",
+          "border: 0px none; width: 100vw; height: 100vh;"
+        );
 
-            // Hide the PRE immediately (until we know what to do, to prevent FOUC)
-            pre.style.display = "none";
-            var isJSON = true;
+        document.body.style.margin = "0px";
+        document.body.style.padding = "0px";
+        document.body.style.overflow = "hidden";
+        document.body.appendChild(editor_node);
 
-            try {
-                const JSONdata = JSON.parse(pre.innerText);
-                chrome.storage.local.set({"JSONExtens1ondata": JSONdata});
-            } catch (error) {
-                isJSON = false;
-                pre.style.display = "initial";
-            }
-
-            if (isJSON) {
-
-                var editor_node = document.createElement('iframe');
-                editor_node.setAttribute("src", chrome.runtime.getURL("editor.html"));
-                editor_node.setAttribute("style", "border: 0px none; width: 100vw; height: 100vh;");
-
-                document.body.style.margin = "0px";
-                document.body.style.padding = "0px";
-                document.body.style.overflow = "hidden";
-                document.body.appendChild(editor_node);
-
-                console.log("editor loaded!");
-            }
-        }
-
+        console.log("editor loaded!");
+      }
     }
+  };
 
-    document.addEventListener("DOMContentLoaded", loadJsonViewer, false);
-
+  document.addEventListener("DOMContentLoaded", loadJsonViewer, false);
 })();
